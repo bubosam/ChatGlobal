436c91872fd7ff3d8a09d866ca7c5a4b9ee19f1f
matuskokoska <matus.kokoska@akademiasovy.sk>
2016-05-03 22:37:39 +0200

    simmple database handler added, few users added to database

--- DreamChat/API/DBconnect.js
+++ DreamChat/API/DBconnect.js
@@ -1,1 +1,39 @@
-﻿//todo
+﻿var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'dreamchat'
+});
+connection.connect();
+
+//TO DO add comments
+
+module.exports = {
+	insert: function(table, object) {
+		var query = connection.query('insert into ' + table + ' set ?', object, function (err, result) {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			console.error(result);
+		});
+	},
+	
+	insert: function(queryParam) {
+		var query = connection.query(queryParam, function (err, result) {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			console.error(result);
+		});
+	},
+	
+	select: function(queryParam) {
+		var query = connection.query(queryParam, function (err, result) {
+			console.log(query.sql);
+		});
+	}
+};
---
+++ DreamChat/database/dreamchat.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Hostiteľ: 127.0.0.1
+-- Čas generovania: Út 03.Máj 2016, 22:36
+-- Verzia serveru: 5.6.17
+-- Verzia PHP: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Databáza: `dreamchat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Štruktúra tabuľky pre tabuľku `tokens`
+--
+
+CREATE TABLE IF NOT EXISTS `tokens` (
+  `userID` int(5) NOT NULL,
+  `token` char(32) NOT NULL,
+  PRIMARY KEY (`userID`),
+  UNIQUE KEY `token` (`token`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Štruktúra tabuľky pre tabuľku `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `userID` int(5) NOT NULL AUTO_INCREMENT,
+  `nickname` varchar(20) DEFAULT NULL,
+  `email` varchar(20) NOT NULL,
+  `password` varchar(16) NOT NULL,
+  PRIMARY KEY (`userID`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `email_2` (`email`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Sťahujem dáta pre tabuľku `users`
+--
+
+INSERT INTO `users` (`userID`, `nickname`, `email`, `password`) VALUES
+(1, 'nemamradgulas', 'vymysleny@email.com', 'hesloheslo'),
+(2, 'mickeymouse', 'mickey@mouse.sk', 'hesloheslo'),
+(3, 'oskar', 'zase@prace.com', 'staralubovna'),
+(4, 'donaldTrumpz', 'fake@donald.com', 'amerikausa'),
+(5, 'dominikkolesar', 'dominik@kolesar.sk', '00000000');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
