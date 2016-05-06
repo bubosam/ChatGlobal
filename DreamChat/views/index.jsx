12e6f1594007426e50033717e1b8e8c19d2342b3
Miloš Kočan <milos.kocan@akademiasovy.sk>
2016-05-06 09:21:46 +0200

    Update index.jsx & master.jsx
    Update index.jsx & master.jsx

--- DreamChat/views/index.jsx
+++ DreamChat/views/index.jsx
@@ -4,13 +4,13 @@ var DefaultLayout = require('./layout/master');
 
 var IndexComponent = React.createClass({
     render: function () {
-        return (
-            <DefaultLayout title = {this.props.title}>
-                 <div>
-                    <h1>Welcome to {this.props.title}!!!</h1>
-                 </div >
+        return (      
+
+            <DefaultLayout title = {this.props.title}>          
+
+
             </DefaultLayout>
-         )
+         );        
     }
 });
 
--- DreamChat/views/layout/master.jsx
+++ DreamChat/views/layout/master.jsx
@@ -1,4 +1,7 @@
-﻿var React = require('react');
+﻿import React from 'react';
+import AppBar from 'material-ui/lib/app-bar';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
 var MasterLayout = React.createClass({
 
@@ -8,16 +11,36 @@ var MasterLayout = React.createClass({
 
             <html xmlns="http://www.w3.org/1999/xhtml">
             <head>
-            <meta charset="utf-8" />
-            <title>{this.props.title}</title>
+                <link rel="stylesheet"  href="css/main.css"/>
+                <script src="http://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js" type="text/javascript" async=""></script>
+                <meta charset="utf-8" />
+                <title>{this.props.title}</title>
             </head>
             <body>
+
+                <div className='header'>
+                    <span className='logo'></span>
+                </div>
+
+                <div className='menu'>
+                    <AppBar title="Dream Chat"
+                        iconClassNameRight="muidocs-icon-navigation-expand-more"
+                        className="app-bar" />
+                </div>
+
+                <div className='login'>
+
+
+
+                </div>
+
                 {this.props.children}
+
             </body>
-            </html>    
-                   
-            )
-    } 
+            </html>
+
+            );
+    }
 
     });
 
