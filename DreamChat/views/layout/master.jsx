86ea7544ff65bad9c974ac745e6d0ffb3a8cb6ff
MatejPliesovsky <matej.pliesovsky@akademiasovy.sk>
2016-05-09 17:13:43 +0200

    webpages vizual format of code

--- DreamChat/views/layout/master.jsx
+++ DreamChat/views/layout/master.jsx
@@ -1,79 +1,95 @@
-﻿import React from 'react';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-injectTapEventPlugin();
+﻿//imports
+var React = require('react');
+var InjectTapEventPlugin = require('react-tap-event-plugin');
 
 var AppBar = require('material-ui/lib/app-bar');
 var TextField = require('material-ui//lib/text-field');
 var RaisedButton = require('material-ui/lib/raised-button');
 
+injectTapEventPlugin();
+
 var MasterLayout = React.createClass({
-
     render: function () {
+        return (      
 
-        return (
-
+           //webpage body
             <html xmlns="http://www.w3.org/1999/xhtml">
-            <head>
-                <link rel="stylesheet"  href="css/main.css"/>
+             <head>//page head with with 
+               
+                 <link rel="stylesheet"  href="css/main.css"/>
                 <script src="http://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js" type="text/javascript" async=""></script>
                 <meta charset="utf-8" />
                 <title>{this.props.title}</title>
-            </head>
+
+              </head>
+
+              //body of page
             <body>
-                <form method="post" action="/signup">
-                <div className='header'>
-                    <span className='logo'></span>
-                </div>
-                <div className='menu'>
-                    <AppBar title="Dream Chat"
+                <form method="post" action="/signin">
+                  
+                  //app logo 
+                    <div className='header'>
+                       <span className='logo'></span>
+                    </div>
+                 
+                  //menu bar
+                    <div className='menu'>
+                     <AppBar title="Dream Chat"
                             iconClassNameRight="muidocs-icon-navigation-expand-more"
                             className="app-bar" />
-                </div>
+                  </div>
 
-                <div className='login'>
-                </div>
-<div>
+                  //login page logo
+                  <div className='login'>
+                  </div>
+      
+             //textfields for username and password 
+              
                   <br /><br />
-<center>
-     <TextField name="username" id="username"
-                placeholder="e-mail" required="true" />
-</center>
-<br />
-</div>
+                   <center>
+                    
+                    <TextField name="username" id="username" placeholder="e-mail" required="true" />
+                   </center>
+                  <br />
+              
 
-                <center>
-<TextField type="password" name="password" id="password"
-           placeholder="password" required="true" />
+                   <center>
+                    <TextField type="password" name="password" id="password" placeholder="password" required="true" />
+                   </center>
+                 <br />
+                 
+          //buttons
+                   <center>
+                    <div>//submit login
+                     <RaisedButton label="Login" secondary={true} type="submit" name="signup" id="signin" value="register" 
+                                  style={{
+                                 margin: 3,
+                                 display: 'block' ,
+                                 width: 10 + '%' ,
+                                 float: 'center'
+                     }} />
+                     </div>
+                   </center>
+       
+                  <center>
+                     <div>//refer to registration page
+                         <RaisedButton label="Register" secondary={true} type="submit" name="signup" id="signup" value="register" 
+                                       style={{
+                                       margin: 3,
+                                       display: 'block' ,
+                                       width: 10 + '%' ,
+                                      float: 'center'
+                                       }} />
+                             <a href= "./register"></a>
+                      </div>
                 </center>
-             <br />
-                    <center>
-  <div>
-<RaisedButton label="Login" secondary={true} type="submit" name="signup" id="signin" value="register" style={{
-              margin: 3,
-              display: 'block' ,
-              width: 10 + '%' ,
-              float: 'center'
-              }} />
-  </div>
-                    </center>
-         <center>
-         <div>
-    <RaisedButton label="Register" secondary={true} type="submit" name="signup" id="signup" value="register" style={{
-                  margin: 3,
-              display: 'block' ,
-              width: 10 + '%' ,
-              float: 'center'
-                  }} /><a href= "./register"></a>
-         </div>
-         </center>
 
-                </form>
-</body>
-            </html>
+                 </form>
+            </body>
+          </html>
 
-            );
+       );
     }
-
-    });
+  });
 
 module.exports = MasterLayout;
--- DreamChat/views/layout/register.jsx
+++ DreamChat/views/layout/register.jsx
@@ -1,82 +1,98 @@
-﻿var React = require('react');
+﻿//inserts
+var React = require('react');
 var Colors = require('material-ui/lib/styles/colors');
 var AppBar = require('material-ui/lib/app-bar');
 var TextField = require('material-ui//lib/text-field');
 var RaisedButton = require('material-ui/lib/raised-button');
 
-var MasterLayout = React.createClass({
-
-
+var RegisterLayout = React.createClass({
     render: function () {
+        return (      
 
-        return (
+            //webpage body                      
+             <html xmlns="http://www.w3.org/1999/xhtml">
+             <head>
+                 //page head with with
 
-            <html xmlns="http://www.w3.org/1999/xhtml">
-            <head>
-                <link rel="stylesheet" href="css/main.css" />
+                 <link rel="stylesheet" href="css/main.css" />
                 <script src="http://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js" type="text/javascript" async=""></script>
                 <meta charset="utf-8" />
-            <title>{this.props.title}</title>
-            </head>
+                <title>{this.props.title}</title>
+
+             </head>
+
+            //body of page
             <body>
-                <form method="post" action="/signup">
-                 <div className='header'>
-                    <span className='logo'></span>
-                 </div>
-               <div className='menu'>
-                    <AppBar title="Dream Chat"
-                            iconClassNameRight="muidocs-icon-navigation-expand-more"
-                            className="app-bar" />
-               </div>
-                 <div className='register'>
-                 </div>
-<div>
-     <br /><br />
-<center>
-     <TextField name="username" id="username"
-                  placeholder="e-mail" required="true"/>
-</center>
-<br />
-<center>
-<TextField type="password" name="password" id="password"
-                   placeholder="password" required="true"/>
-</center>
-             <br />
-             <center>
-<TextField type="password" name="password" id="password"
-                   placeholder="password" required="true"/>
-             </center>
-             <br />
-</div>
+                <form method="post" action="/signin">
 
-<center>
-  <div>
-<RaisedButton label="Register" secondary={true} type="submit" name="signup" id="signup" value="register" style={{
-    margin: 3,
-    display: 'block',
-    width: 10 + '%',
-    float: 'center'
-}} />
-  </div>
-</center>
-         <center>
-         <div>
-    <RaisedButton label="Cancel" secondary={true} style={{
-    margin: 3,
-    display: 'block',
-    width: 10 + '%',
-    float: 'center'
-}}/><a href="/master" ></a>
-         </div>
-         </center>
+                    //app logo
+                    <div className='header'>
+                       <span className='logo'></span>
+                    </div>
 
-                </form>
-</body>
-   </html>    
+                    //menu bar
+                    <div className='menu'>
+                     <AppBar title="Dream Chat"
+                             iconClassNameRight="muidocs-icon-navigation-expand-more"
+                             className="app-bar" />
+                    </div>
+
+                    //login page logo
+                   <div className='register'>
+                  </div>
+
+                    //textfields for username and password
+              
+                  <br /><br />
+                   <center>
+
+                    <TextField name="username" id="username" placeholder="e-mail" required="true" />
+                   </center>
+                  <br />
+              
+
+                   <center>
+                    <TextField type="password" name="password" id="password" placeholder="password" required="true" />
+                   </center>
+                 <br />
+
+                  <center>
+                    <TextField type="re-enter password" name="password" id="password" placeholder="re-enter password" required="true" />
+                 </center>
+                <br />
+
+             //buttons
+                  <center>
+                   <div>//submit registration
+                     <RaisedButton label="Register" secondary={true} type="submit" name="signup" id="signup" value="register" 
+                                   style={{
+                                   margin: 3,
+                                   display: 'block',
+                                   width: 10 + '%',
+                                   float: 'center'
+                     }} />
+                   </div>
+                </center>
+                
+               <center>
+                 <div>//refer to login
+                     <RaisedButton label="Back" secondary={true} 
+                                   style={{
+                                   margin: 3,
+                                   display: 'block',
+                                   width: 10 + '%',
+                                   float: 'center'
+                     }} />
+                     <a href="/master"></a>
+                </div>
+              </center>
+
+               </form>
+            </body>
+          </html>    
                    
-            )
-} 
-
+          )
+        }
 });
 
 module.exports = MasterLayout;
