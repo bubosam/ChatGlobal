5f860e6759747ab998012fc0542ca17c3b5ecf10
branislavdavid <branislav.david@akademiasovy.sk>
2016-05-10 11:50:42 +0200

    regex.js created

---
+++ DreamChat/API/regex.js
@@ -0,0 +1,15 @@
+﻿
+var Regex = require("regex");
+
+module.exports = {
+    controlEmail: function (email) {
+        var regexEmail = new Regex(/^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i);
+        regexEmail.test(email);
+    },
+
+    controlPassword: function (password) {
+        var regexPassword = new Regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/);
+        regexPassword.test(password);
+    }
+
+};
--- DreamChat/routes/register.js
+++ DreamChat/routes/register.js
@@ -1,16 +1,20 @@
 ﻿var express = require('express');
 var router = express.Router();
 var bodyParser = require("body-parser");
+var regex = require(appRoot + '/API/regex.js');
 
 router.post('/', function (req, res) {
 	var email = req.query.email;
 	var password = req.query.password;
-	var nickname = req.query.nickname;
-	var reg = require(appRoot + "/API/register");
-	reg.register(nickname, email, password, function (success) {
-		console.log(success);
-		res.json(success);
-	});
+    var nickname = req.query.nickname;
+    
+    if (regex.controlEmail(email) && regex.controlPassword(password)) {
+        var reg = require(appRoot + "/API/register");
+        reg.register(nickname, email, password, function (success) {
+            console.log(success);
+            res.json(success);
+        });
+    }
 });
 
 
