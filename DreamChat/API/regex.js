8da59c944659f7eb3dd7b64cadefce886d53d582
branislavdavid <branislav.david@akademiasovy.sk>
2016-05-11 15:05:53 +0200

    regex progress

--- DreamChat/API/regex.js
+++ DreamChat/API/regex.js
@@ -1,15 +1,27 @@
-﻿
-var Regex = require("regex");
+﻿var Regex = require("regex");
 
 module.exports = {
     controlEmail: function (email) {
-        var regexEmail = new Regex(/^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i);
-        regexEmail.test(email);
+       // var regexEmail = new Regex("/^(([^<>()\\[\\]\\.,;:\s@\"]+(\.[^<>()\[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/");
+        /*if (typeof callback === "function") {
+            callback(regexEmail.test(email));
+        }*/
+        //return regexEmail.test(email);
+        var re = /^([A-Z|a-z|0-9](\.|_){0,1})+[A-Z|a-z|0-9]\@([A-Z|a-z|0-9])+((\.){0,1}[A-Z|a-z|0-9]){2}\.[a-z]{2,3}$/;
+        console.log("ëmail :"+re.test(email));
+        return re.test(email);
+
     },
+    
 
     controlPassword: function (password) {
-        var regexPassword = new Regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/);
-        regexPassword.test(password);
-    }
-
+        //var regexPassword = new Regex(/\w{8,16}/);
+           /* if (typeof callback === "function") {
+                callback(regexPassword.test(password));
+            }   */
+          // return regexPassword.test(password);
+            var re = /\w{8,16}/;
+            console.log("passwôrd :" + re.test(password));
+            return re.test(password);
+        }
 };
