bccece18b2e63e28e0f372399119dfeb7a25feec
branislavdavid <branislav.david@akademiasovy.sk>
2016-05-12 13:34:36 +0200

    server - register working , DATABASE upgraded!!!

--- DreamChat/database/dreamchat.sql
+++ DreamChat/database/dreamchat.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Hostiteľ: 127.0.0.1
--- Čas generovania: Pi 06.Máj 2016, 10:06
+-- Čas generovania: Št 12.Máj 2016, 11:33
 -- Verzia serveru: 5.7.9
 -- Verzia PHP: 5.6.16
 
@@ -23,6 +23,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Štruktúra tabuľky pre tabuľku `friendships`
+--
+
+DROP TABLE IF EXISTS `friendships`;
+CREATE TABLE IF NOT EXISTS `friendships` (
+  `friendshipID` int(11) NOT NULL AUTO_INCREMENT,
+  `user1` int(11) NOT NULL,
+  `user2` int(11) NOT NULL,
+  PRIMARY KEY (`friendshipID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Štruktúra tabuľky pre tabuľku `friend_requests`
+--
+
+DROP TABLE IF EXISTS `friend_requests`;
+CREATE TABLE IF NOT EXISTS `friend_requests` (
+  `requestID` int(11) NOT NULL AUTO_INCREMENT,
+  `sender` int(11) NOT NULL,
+  `receiver` int(11) NOT NULL,
+  PRIMARY KEY (`requestID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
 -- Štruktúra tabuľky pre tabuľku `tokens`
 --
 
@@ -38,7 +66,8 @@ CREATE TABLE IF NOT EXISTS `tokens` (
 --
 
 INSERT INTO `tokens` (`userID`, `token`) VALUES
-(3, 'a019ed400268a575b4638727d8f2b4');
+(3, 'a019ed400268a575b4638727d8f2b4'),
+(2, '86b36a19041b9b7401523fb05371c4');
 
 -- --------------------------------------------------------
 
@@ -52,21 +81,30 @@ CREATE TABLE IF NOT EXISTS `users` (
   `nickname` varchar(20) DEFAULT NULL,
   `email` varchar(254) NOT NULL,
   `password` varchar(16) NOT NULL,
+  `phone` char(15) DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `surname` varchar(20) DEFAULT NULL,
+  `avatar` char(1) DEFAULT NULL,
   PRIMARY KEY (`userID`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `email_2` (`email`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
 --
 -- Sťahujem dáta pre tabuľku `users`
 --
 
-INSERT INTO `users` (`userID`, `nickname`, `email`, `password`) VALUES
-(1, 'nemamradgulas', 'vymysleny@email.com', 'hesloheslo'),
-(2, 'mickeymouse', 'mickey@mouse.sk', 'hesloheslo'),
-(3, 'oskar', 'zase@prace.com', 'staralubovna'),
-(4, 'donaldTrumpz', 'fake@donald.com', 'amerikausa'),
-(5, 'dominikkolesar', 'dominik@kolesar.sk', '00000000');
+INSERT INTO `users` (`userID`, `nickname`, `email`, `password`, `phone`, `name`, `surname`, `avatar`) VALUES
+(1, 'nemamradgulas', 'vymysleny@email.com', 'hesloheslo', NULL, NULL, NULL, NULL),
+(2, 'mickeymouse', 'mickey@mouse.sk', 'hesloheslo', NULL, NULL, NULL, NULL),
+(3, 'oskar', 'zase@prace.com', 'staralubovna', NULL, NULL, NULL, NULL),
+(4, 'donaldTrumpz', 'fake@donald.com', 'amerikausa', NULL, NULL, NULL, NULL),
+(5, 'dominikkolesar', 'dominik@kolesar.sk', '00000000', NULL, NULL, NULL, NULL),
+(6, 'breanko1', 'branko@centrum.sk', 'mrkvicka', NULL, NULL, NULL, NULL),
+(7, 'marekGamer111', 'karol@bandaska.hu', 'marekjelama123', NULL, NULL, NULL, NULL),
+(8, 'svetovymier', 'jan@pavol.eu', 'nechzijeboh', NULL, NULL, NULL, NULL),
+(9, 'lubko', 'lubomir@biely.eu', 'YOLOLOLO', NULL, NULL, NULL, NULL),
+(10, 'netreba', 'ten@koho.eu', 'menovat123', NULL, NULL, NULL, NULL);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
