4a168cdba823843d64753b164b223ca808d806ee
MonikaJassova <monika.jassova@akademiasovy.sk>
2016-05-12 12:59:25 +0200

    friends fragment layout started

--- Android/DreamChat/app/src/main/java/com/example/boush/dreamchat/FriendsFragment.java
+++ Android/DreamChat/app/src/main/java/com/example/boush/dreamchat/FriendsFragment.java
@@ -1,11 +1,21 @@
 package com.example.boush.dreamchat;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -18,12 +28,123 @@ public class FriendsFragment extends Fragment {
         // Required empty public constructor
     }
 
+    Context context;
+    private List<Friend> friendList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private FriendsAdapter mAdapter;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friends, container, false);
+        View view = inflater.inflate(R.layout.fragment_friends, container, false);
+        context = getActivity();
+
+        recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view_friends);
+
+        mAdapter = new FriendsAdapter(friendList);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(context);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.addItemDecoration(new DividerItemDecoration(context, LinearLayoutManager.VERTICAL));
+
+        recyclerView.setAdapter(mAdapter);
+
+        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(context, recyclerView, new ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Friend friend = friendList.get(position);
+                Toast.makeText(context, friend.getTitle() + " is selected!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));
+
+        prepareFriendData();
+        return view;
+    }
+
+    private void prepareFriendData() {
+        Friend friend = new Friend("Iba", "Meliško", "Slovensko", "11.5.2016");
+        friendList.add(friend);
+
+        friend = new Friend("Patres", "Patinák", "DreamTeam", "12.5.2016");
+        friendList.add(friend);
+
+        friend = new Friend("Martin", "Tarhanič", "Global Logic", "10.5.2016");
+        friendList.add(friend);
+
+        friend = new Friend("Monika", "Jaššová", "DreamTeam", "12.5.2016");
+        friendList.add(friend);
+
+        friend = new Friend("Michal", "Borovský", "DreamTeam", "12.5.2016");
+        friendList.add(friend);
+
+        friend = new Friend("Matúš", "Kokoška", "DreamTeam", "11.5.2016");
+        friendList.add(friend);
+
+        friend = new Friend("Roman", "Klimčík", "Global Logic", "10.4.2016");
+        friendList.add(friend);
+
+        friend = new Friend("X", "Y", "Slovensko", "12.5.2016");
+        friendList.add(friend);
+
+        friend = new Friend("Meno", "Priezvisko", "Skupina", "10.5.2016");
+        friendList.add(friend);
+
+        mAdapter.notifyDataSetChanged();
+    }
+
+    public interface ClickListener {
+        void onClick(View view, int position);
+
+        void onLongClick(View view, int position);
+    }
+
+    public static class RecyclerTouchListener implements RecyclerView.OnItemTouchListener {
+
+        private GestureDetector gestureDetector;
+        private FriendsFragment.ClickListener clickListener;
+
+        public RecyclerTouchListener(Context context, final RecyclerView recyclerView, final FriendsFragment.ClickListener clickListener) {
+            this.clickListener = clickListener;
+            gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+                    return true;
+                }
+
+                @Override
+                public void onLongPress(MotionEvent e) {
+                    View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
+                    if (child != null && clickListener != null) {
+                        clickListener.onLongClick(child, recyclerView.getChildPosition(child));
+                    }
+                }
+            });
+        }
+
+        @Override
+        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+
+            View child = rv.findChildViewUnder(e.getX(), e.getY());
+            if (child != null && clickListener != null && gestureDetector.onTouchEvent(e)) {
+                clickListener.onClick(child, rv.getChildPosition(child));
+            }
+            return false;
+        }
+
+        @Override
+        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+        }
+
+        @Override
+        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+        }
     }
 
 }
