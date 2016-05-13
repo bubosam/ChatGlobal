aaf4b844babf570f76175979cfb2afcef4794f54
MonikaJassova <monika.jassova@akademiasovy.sk>
2016-05-13 12:37:47 +0200

    tabSelectedListener

--- Android/DreamChat/app/src/main/java/com/example/boush/dreamchat/MenuActivity.java
+++ Android/DreamChat/app/src/main/java/com/example/boush/dreamchat/MenuActivity.java
@@ -37,6 +37,22 @@ public class MenuActivity extends AppCompatActivity {
         pager.setAdapter(adapter);
 
         pager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                pager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 
 
