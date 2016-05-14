b91ee4ec12224778c9a9b0a55105266fff2f59be
matuskokoska <matus.kokoska@aademiasovy.sk>
2016-05-14 17:44:43 +0200

    users handler added

---
+++ DreamChat/API/users.js
@@ -0,0 +1,23 @@
+﻿var db = require('./DBconnect.js');
+
+module.exports = {
+    load: function (userid,callback) {
+        db.query("SELECT FROM users WHERE userid="+userid+"",
+        function (result) {
+            if (typeof callback === "function") {
+                callback(result[0]);
+            }
+        });
+    },
+
+    updateName: function (user,callback) {
+        db.nonQuery("UPDATE users SET nickname:'" + user.nickname + "', password:'" + user.password +
+                    "', email:'" + user.email + "', phone:'" + user.phone + "', name:'" + user.name +
+                    "', surname:'" + user.surname + "', ",
+        function (success) {
+            if (typeof callback === "function") {
+                callback(success);
+            }
+        });
+    },
+};
