8dc0f56de49256c88eb58f3e87a9b6b7d0e65916
mareksibal <marek.sibal@akademiasovy.sk>
2016-05-19 11:58:33 +0200

    Text/Button [test]

--- DreamChat/public/components/DashBoardComp.jsx
+++ DreamChat/public/components/DashBoardComp.jsx
@@ -2,13 +2,14 @@ var React = require('react');
 var PropTypes = React.PropTypes;
 var {Link} = require('react-router');
 
+
 var DashBoardComp = React.createClass({
 
 
   render: function() {
     return (
       <div>
-
+      <input type="text" name="title" /><input type="button" name="searchbutton" value="Search" onClick="search()" />
       </div>
     );
   }
