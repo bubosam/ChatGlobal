6ea8d4b177cdff573cd7c0787739d82e2c5e1515
matuskokoska <matus.kokoska@aademiasovy.sk>
2016-05-19 14:26:03 +0200

    contact request http requests and responds modified, authorization added, error handling

---
+++ DreamChat/API/authorization.js
@@ -0,0 +1,12 @@
+﻿var login = require(appRoot + "/API/login");
+
+module.exports = {
+    authorize: function (req, callback){
+        var userid = req.headers.userid;
+        var token = req.headers.token;
+        login.access(userid, token, function (success) {
+            console.log(success);
+            callback(success);
+        });
+    }
+}
--- DreamChat/routes/requests.js
+++ DreamChat/routes/requests.js
@@ -2,49 +2,106 @@
 var router = express.Router();
 var bodyParser = require("body-parser");
 var requests = require(appRoot + "/API/requests");
+var authorization = require(appRoot + "/API/authorization");
 
 
 //sending sender id and receiver id to database and creating row with requestid information
-router.post('/send', function (req, res) {
-    var sender = req.body.userid;
-    var receiver = req.body.receiver;
-
-    requests.sendRequest(sender, receiver, function (success) {
-        console.log(success);
-        res.json(success);
-    });	
+router.post('/', function (req, res) {
+    authorization.authorize(req, function (access) { 
+        if (access) {
+            var sender = req.headers.userid;
+            var receiver = req.body.receiver;
+            
+            requests.sendRequest(sender, receiver, function (success,code) {
+                if (success) {
+                    res.json({
+                        "message": "request sent successfully"
+                    });
+                }
+                else {
+                    res.json({
+                        "message": "sending request failed, please try again later"
+                    });
+                }
+            });
+        }
+        else {
+            res.json({
+                "message": "authorization failed"
+            });
+            code = 401;
+        }
+        res.statusCode = code;
+    });    
 });
 
 //delete row from database where there is a requestid
-router.delete('/cancel', function (req, res) {
-    var requestid = req.body.requestid;
-    requests.cancelRequest(requestid, function (success) {
-        console.log(success);
-        res.json(success);
+router.delete('/', function (req, res) {
+    authorization.authorize(req, function (access) {
+        if (access) {
+            var requestid = req.body.requestid;
+            requests.cancelRequest(requestid, function (success) {
+                if (success) {
+                    res.json({
+                        "message": "request canceled successfully",
+                        "error": 0,
+                        "success": true
+                    });
+                }
+                else {
+                    res.json({
+                        "message": "deleting request failed, please try again later",
+                        "error": 1,
+                        "success": false
+                    });
+                }
+            });
+        }    
+        else {
+            res.json({
+                "message": "authorization failed",
+                "error": 2,
+                "success": false
+            });
+        }
     });
 });
 
 //adding friend to table friendships and deleting request from friend_requests
 router.post('/accept', function (req, res) {
-    var requestid = req.body.requestid;
-    requests.acceptRequest(requestid, function (success) {
-        console.log(success);
-        res.json(success);
-    });
-});
-
-//delete request from table friend_requests
-router.delete('/decline', function (req, res) {
-    var requestid = req.body.requestid;
-    requests.cancelRequest(requestid, function (success) {
-        console.log(success);
-        res.json(success);
+    authorization.authorize(req, function (access) {
+        if (access) {
+            var requestid = req.body.requestid;
+            requests.acceptRequest(requestid, function (success) {
+                if (success) {
+                    res.json({
+                        "message": "friend successfully added",
+                        "error": 0,
+                        "success": true
+                    });
+                }
+                else {
+                    res.json({
+                        "message": "accepting request failed, please try again later",
+                        "error": 1,
+                        "success": false
+                    });
+                }
+            });
+        }    
+        else {
+            res.json({
+                "message": "authorization failed",
+                "error": 2,
+                "success": false
+            });
+        }
     });
 });
 
 //load pending requests from table friend_requests
-router.post('/load', function (req, res) {
-    var userid = req.body.userid;
+router.get('/', function (req, res) {
+    var userid = req.headers.userid;
     requests.loadRequests(userid, function (results) {
         res.json(results);
     });
