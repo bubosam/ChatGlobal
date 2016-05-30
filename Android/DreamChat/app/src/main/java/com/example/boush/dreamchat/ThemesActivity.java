366091e7ccd4144bf9c4e46a2d73e7b3bcd27e7e
MonikaJassova <monika.jassova@akademiasovy.sk>
2016-05-30 08:32:36 +0200

    Patrik - Themes

---
+++ Android/DreamChat/app/src/main/java/com/example/boush/dreamchat/ThemesActivity.java
@@ -0,0 +1,38 @@
+package com.example.boush.dreamchat;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ThemesActivity extends BaseActivity implements View.OnClickListener {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_themes);
+        ((Button)findViewById(R.id.blue_theme_btn)).setOnClickListener(this);
+        ((Button)findViewById(R.id.red_theme_btn)).setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.blue_theme_btn:
+                Utility.setTheme(getApplicationContext(), 1);
+                recreateActivity();
+                break;
+            case R.id.red_theme_btn:
+                Utility.setTheme(getApplicationContext(), 2);
+                recreateActivity();
+                break;
+        }
+    }
+    public void recreateActivity() {
+        Intent intent = getIntent();
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        finish();
+        overridePendingTransition(0, 0);
+        startActivity(intent);
+        overridePendingTransition(0, 0);
+    }
+}
