d7ef2b000ef6ab94751d6191cee4fc74df9ab821
matuskokoska <matus.kokoska@aademiasovy.sk>
2016-05-31 09:45:40 +0200

    users search fixed

--- DreamChat/API/users.js
+++ DreamChat/API/users.js
@@ -23,31 +23,25 @@ module.exports = {
 
 	search: function (name, callback) {
     var substrings = name.split(" ");
-    var allResults = [];
+    //db.query("SELECT userid,name,surname,nickname FROM users WHERE name LIKE '%"+substrings[i]+"%' OR surname LIKE '%" + substrings[i] + "%' OR nickname LIKE '%"
+        //      + substrings[i] + "%' OR email LIKE '%" + substrings[i] + "%'",  function (results)
+    var queryString="SELECT userid,name,surname,nickname FROM users WHERE "
     for (var i = 0, len = substrings.length; i < len; i++){
       (function(i){
-        db.query("SELECT userid,name,surname,nickname FROM users WHERE name LIKE '%"+substrings[i]+"%' OR surname LIKE '%" + substrings[i] + "%' OR nickname LIKE '%"
-                  + substrings[i] + "%' OR email LIKE '%" + substrings[i] + "%'",  function (results) {
-                allResults=allResults.concat(results);
-                //console.log(results);
-                //console.log(allResults);
-                console.log(i);
-                if (typeof callback === "function" && i+1==len) {
-                    console.log("tu");
-                    var uniqueUsers = [];
-                    var uniqueNicknames = [];
-                    for(j = 0; j< allResults.length; j++){
-                        console.log(allResults[j].nickname);
-                        if(uniqueNicknames.indexOf(allResults[j].nickname === -1)){
-                            uniqueNicknames.push(allResults[j].nickname);
-                            uniqueUsers.push(allResults[j]);
-                            //console.log(uniqueNicknames);
-                        }
-                    }
-                    console.log(uniqueNicknames);
-                    callback(uniqueUsers);
-                }
-        });
+         queryString+="(name LIKE '%"+substrings[i]+"%' OR surname LIKE '%" + substrings[i]
+          + "%' OR nickname LIKE '%"+ substrings[i] + "%' OR email LIKE '%" + substrings[i] + "%')";
+          if(i+1<len){
+            queryString+=" or ";
+          }
+          else{
+            console.log(queryString);
+            db.query(queryString,function (results){
+              if (typeof callback === "function") {
+                  callback(results);
+              }
+            });
+          }
+      //  });
   		})(i);
     }
 	},
