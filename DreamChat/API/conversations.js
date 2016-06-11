369ecab9256857ca0dc279b4fc0ce0b3b4bec036
matuskokoska <matus.kokoska@akademiasovy.sk>
2016-06-11 18:45:31 +0200

    conversation handler update

--- DreamChat/API/conversations.js
+++ DreamChat/API/conversations.js
@@ -1,23 +1,30 @@
 var db = require('./DBconnect.js');
 
 module.exports = {
-  new: function(user1, user2, callback){
+  newMessage: function(user1, user2, message, callback){
     var code;
     db.query("SELECT * FROM conversations WHERE (user1="+user1+" AND user2="+user2+") OR (user1="+user2+" AND user2="+user1+")", function(result){
       if(result.length>0){
-        if (typeof callback === "function") {
-            callback(false,409);
-        }
+        db.nonQuery("INSERT INTO messages(coversaionid,userid,message) VALUES("+result.conversationid+","+user1+",'"+message+"')",function(success){
+          if (typeof callback === "function") {
+              callback(success);
+          }
+        });
       }
       else{
         db.nonQuery("INSERT INTO conversations(user1,user2) VALUES("+user1+","+user2+")",function(success){
-          if (typeof callback === "function") {
-              if(success){
-                callback(true,200);
+          if(success){
+            db.nonQuery("INSERT INTO messages(coversaionid,userid,message) VALUES((SELECT conversationid FROM conversations ORDER BY conversationid LIMIT1),"
+                        +user1+",'"+message+"')",function(success2){
+              if (typeof callback === "function") {
+                  callback(success2);
               }
-              else{
-                callback(false,500);
-              }
+            });
+          }
+          else{
+            if (typeof callback === "function") {
+                callback(false);
+            }
           }
         });
       }
