43e9e4d48c8f58cd31a14a0127d81db257d999b3
matuskokoska <matus.kokoska@akademiasovy.sk>
2016-06-20 08:58:42 +0200

    accept request method fixed

--- DreamChat/app.js
+++ DreamChat/app.js
@@ -1,4 +1,4 @@
-﻿var express = require('express');
+ ﻿var express = require('express');
 var path = require('path');
 global.appRoot = path.resolve(__dirname);
 var favicon = require('serve-favicon');
