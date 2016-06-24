56ad441048944c48d60d7ddeaffb511996b1a5aa
matuskokoska <matus.kokoska@akademiasovy.sk>
2016-06-24 10:02:23 +0200

    get conversations fixed

--- DreamChat/src/server/API/conversations.js
+++ DreamChat/src/server/API/conversations.js
@@ -43,8 +43,8 @@ module.exports = {
   },
 
   get: function(userid, callback){
-    db.query("SELECT * FROM conversations LEFT JOIN messages ON messages.conversationid=conversations.conversationid "+
-              "WHERE user1="+userid+" OR user2="+userid+" ORDER BY messageid DESC", function(results){
+    db.query("SELECT * FROM conversations inner JOIN messages ON messages.conversationid=conversations.conversationid "+
+             "WHERE (user1="+userid+" OR user2="+userid+")AND message=(SELECT message FROM messages ORDER BY messageid DESC LIMIT 1) GROUP BY conversations.conversationid ORDER BY messageid DESC", function(results){
           if (typeof callback === "function") {
                 callback(results);
           }
